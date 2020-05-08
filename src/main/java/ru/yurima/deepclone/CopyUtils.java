97094337eac5d94a7da7e2bc993b3a141b61ecf7
Yury <edenstack17@gmail.com>
2020-05-08 17:31:59 +0400

    add creating new instance

--- src/main/java/ru/yurima/deepclone/CopyUtils.java
+++ src/main/java/ru/yurima/deepclone/CopyUtils.java
@@ -1,20 +1,46 @@
 package ru.yurima.deepclone;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CopyUtils {
-    public static <T> T deepCopy(T object) {
+    public static <T> T deepCopy(final T object) {
         try {
-            return (T) object.getClass().getDeclaredConstructor().newInstance();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
+            T copy = createNewInstance(object);
+            return copy;
+
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
         }
         return null;
     }
+
+    private static <T> T createNewInstance(T object) throws IllegalAccessException,
+            InvocationTargetException, InstantiationException {
+        Constructor<T> constructor = (Constructor<T>)
+                object.getClass().getDeclaredConstructors()[0];
+        List<Object> params = getParamsList(constructor);
+        T instance = constructor.newInstance(params.toArray());
+        return instance;
+    }
+
+    private static List<Object>  getParamsList(Constructor constructor) {
+        List<Object> params = new ArrayList<>();
+        for (Class type : constructor.getParameterTypes()) {
+            if (type.isPrimitive()) {
+                if (type == boolean.class) {
+                    params.add(false);
+                } else {
+                    params.add(0);
+                }
+            } else {
+                params.add(null);
+            }
+        }
+        return params;
+    }
+
 }
--- src/test/java/ru/yurima/deepclone/CopyUtilsTest.java
+++ src/test/java/ru/yurima/deepclone/CopyUtilsTest.java
@@ -10,6 +10,10 @@ public class CopyUtilsTest {
     public void deepCopy() {
         Man man = new Man("John", 20, Arrays.asList("Martian"));
         Man copy = CopyUtils.deepCopy(man);
+        System.out.printf("Original Name = %s, age = %d, books = %s\n", man.getName(), man.getAge(),
+                man.getFavoriteBooks().toString());
+        System.out.printf("Copy Name = %s, age = %d, books = %s\n", copy.getName(), copy.getAge(),
+                copy.getFavoriteBooks()!= null ? copy.getFavoriteBooks().toString() : "none");
         assert(man != copy);
 
     }
